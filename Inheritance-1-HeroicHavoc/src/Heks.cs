f9740f4dd61b5b99cd418b31e7b88efdd0e38e88
y1lmaz-Ahmet <ay0201998@gmail.com>
2023-04-23 19:51:48 +0200

    inheritance Troep class in Heks class

--- Inheritance-1-HeroicHavoc/src/Heks.cs
+++ Inheritance-1-HeroicHavoc/src/Heks.cs
@@ -6,7 +6,24 @@ using System.Threading.Tasks;
 
 namespace Inheritance_1_HeroicHavoc.src
 {
-    internal class Heks
+    internal class Heks : Troep
     {
+        //inheritance the props from Troep "Troop" class
+        public Heks(double strength, int healthPoints, string name, double movementSpeed)
+            : base(strength, healthPoints, name, movementSpeed)
+        {
+        }
+        public void Appear()
+        {
+            Console.WriteLine("Here I am!");
+        }
+        public void Hide()
+        {
+            Console.WriteLine("Now you cannot see me anymore!");
+        }
+        public override string ToString()
+        {
+            return base.ToString()+$" [Heks]";
+        }
     }
 }
