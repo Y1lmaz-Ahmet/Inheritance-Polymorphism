330f9fadedf4f9a0129728602336647e027b038c
y1lmaz-Ahmet <ay0201998@gmail.com>
2023-04-23 20:35:38 +0200

    trying to make a class diagram for project - inheritance

---
+++ Class Diagram - Polymorphism - Cat And Dog/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_Diagram___Polymorphism___Cat_And_Dog
+{
+    public class Class1
+    {
+    }
+}
