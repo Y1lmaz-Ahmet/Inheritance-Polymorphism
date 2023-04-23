81cbd9783e01212e7c1c21efc1b5bf15fe8484b2
y1lmaz-Ahmet <ay0201998@gmail.com>
2023-04-23 19:58:34 +0200

    Add Goblin class inherited from Troop class

--- Inheritance-1-HeroicHavoc/src/Goblin.cs
+++ Inheritance-1-HeroicHavoc/src/Goblin.cs
@@ -6,7 +6,22 @@ using System.Threading.Tasks;
 
 namespace Inheritance_1_HeroicHavoc.src
 {
-    internal class Goblin
+    internal class Goblin : Troep
     {
+        public int GatheredGoldPoints { get; set; }
+        public Goblin(double strength, int healthPoints, string name, double movementSpeed) 
+            : base(strength, healthPoints, name, movementSpeed)
+        {
+            GatheredGoldPoints = 0;
+        }
+        public void StealGold()
+        {
+            GatheredGoldPoints += 100;
+            Console.WriteLine("I have stolen 100 gold!");
+        }
+        public override string ToString()
+        {
+            return base.ToString()+$" [Goblin]{GatheredGoldPoints}";
+        }
     }
 }
