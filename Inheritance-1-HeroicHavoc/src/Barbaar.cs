a64afb7274dee8a8fd9e8041ff92a303d5310177
y1lmaz-Ahmet <ay0201998@gmail.com>
2023-04-23 19:55:39 +0200

    Add Barbarian class inherited from Troop class

--- Inheritance-1-HeroicHavoc/src/Barbaar.cs
+++ Inheritance-1-HeroicHavoc/src/Barbaar.cs
@@ -6,7 +6,21 @@ using System.Threading.Tasks;
 
 namespace Inheritance_1_HeroicHavoc.src
 {
-    internal class Barbaar
+    internal class Barbaar : Troep
     {
+        //inheritance Troep class in Barbaar
+        public Barbaar(double strength, int healthPoints, string name, double movementSpeed) 
+            : base(strength, healthPoints, name, movementSpeed)
+        {
+        }
+        public void GetWild()
+        {
+            Console.WriteLine("Drinking Super Strength potion!\nNow I am Much stronger!");
+        }
+        public override string ToString()
+        {
+            return base.ToString()+$" [Barbaar]";
+        }
+
     }
 }
