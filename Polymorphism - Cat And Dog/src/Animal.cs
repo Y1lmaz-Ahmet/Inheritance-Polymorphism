f7fe2bd5c2f8a67c4a686b962bb5cf1159a7b11c
y1lmaz-Ahmet <ay0201998@gmail.com>
2023-04-24 11:09:24 +0200

    Animal Class

--- Polymorphism - Cat And Dog/src/Animal.cs
+++ Polymorphism - Cat And Dog/src/Animal.cs
@@ -8,5 +8,25 @@ namespace Polymorphism___Cat_And_Dog.src
 {
     internal class Animal
     {
+        public string Name { get; set; }
+
+        public Animal(string name)
+        {
+            Name = name;
+        }
+        //We use virtual when a method may (but must not) be overridden
+        public virtual void MakeNoise()
+        {
+            Console.WriteLine("GRRRR");
+        }
+        //We use virtual when a method may(but must not) be overridden
+        public virtual void Eat()
+        {
+            Console.WriteLine("eating food");
+        }
+        public override string ToString()
+        {
+            return $"[Animal]: {Name}";
+        }
     }
 }
