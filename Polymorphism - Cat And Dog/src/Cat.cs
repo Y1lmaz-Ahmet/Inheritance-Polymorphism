71f72294d3404e2a1cd846d7baceca39714c4e01
y1lmaz-Ahmet <ay0201998@gmail.com>
2023-04-24 11:34:51 +0200

    cat class override & new methods

--- Polymorphism - Cat And Dog/src/Cat.cs
+++ Polymorphism - Cat And Dog/src/Cat.cs
@@ -6,7 +6,26 @@ using System.Threading.Tasks;
 
 namespace Polymorphism___Cat_And_Dog.src
 {
-    internal class Cat
+    internal class Cat : Animal
     {
+        public Cat(string name) : base(name)
+        {
+        }
+        public new void MakeNoise()
+        {
+            base.MakeNoise();
+            Console.WriteLine("Miauwww");
+        }
+        //we use override to indicate that we want
+        //to change the implementation of the method
+        public override void Eat()
+        {
+            base.Eat();
+            Console.WriteLine("Eating Mice, Yummy!");
+        }
+        public override string ToString()
+        {
+            return $"[Cat]: {Name}";
+        }
     }
 }
